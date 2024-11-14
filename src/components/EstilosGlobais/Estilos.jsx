f465737dd56d94eefa246048d57a51688c71ea00
Bruno Furtado <bsouzacc@gmail.com>
2024-11-14 11:36:03 -0300

    update /Instalação de Emotion

---
+++ src/components/EstilosGlobais/Estilos.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { css, Global } from "@emotion/react"
+
+const estilos = css`
+   *{
+        font-family: "Montserrat", sans-serif;
+    }
+`
+
+export const Estilos = () => {
+    return (
+        <Global
+            styles={estilos}
+        />
+    )
+}
