3eb74e145f1498a4741eafb2e278daaf2d34a773
Bruno Furtado <bsouzacc@gmail.com>
2024-11-18 11:57:21 -0300

    update /lista suspensa style

--- src/components/ListaSuspensa/ListaSuspensa.jsx
+++ src/components/ListaSuspensa/ListaSuspensa.jsx
@@ -35,50 +35,77 @@ const BotaoEstilizado = styled.button`
     border: 1px solid ${props => props.theme.cores.neutras.a};
     &:focus {
         border-color: ${props => props.theme.cores.focus};
-    }
+    };
 `
 const ListaSuspensaEstilizada = styled.ul`
-    width: 100%;
-    padding: 0px;
-    margin: 0px;
-    text-align: center;
+   position: absolute;
+    top: 100%;
+    left: 0;
+    right: 0;
+    background-color: ${props => props.theme.cores.branco};
+    z-index: 1;
+    border: 1px solid ${props => props.theme.cores.neutras.a};
+    border-bottom-left-radius: 18px;
+    border-bottom-right-radius: 18px;
+    border-top: none;
+    margin: 0;
+    padding: 0 ${props => props.theme.espacamentos.m};
     list-style: none;
-    background: ${props => props.theme.cores.branco};
 `
 const ItemListaSuspensaEstilizada = styled.li`
-    border: 1px solid black;
-    padding: 4px;
-    background: ${props => props.theme.cores.branco};
-    font-weight: 500;
-    color: ${props => props.focoAtivo ? props.theme.cores.focus : 'inherit'};
+    padding: ${props => props.theme.espacamentos.xs} 0;
+    text-align: center;
+    border-bottom: 1px solid ${props => props.theme.cores.neutras.c};
+    cursor: pointer;       
+    &:last-child {
+        border: none;
+    };
     &:hover {
         color: ${props => props.theme.cores.focus};
-    }
+    };
 `
 
 export const ListaSuspensa = ({ label, opcoes }) => {
-
     const [estaAberta, alternarVisibilidade] = useState(false);
 
     const [opcaoFocada, setOpcaoFocada] = useState(null);
+    const [opcaoSelecionada, setOpcaoSelecionada] = useState(null);
 
     const manipularTeclaDoTeclado = (evento) => {
 
         alternarVisibilidade(true)
 
         switch (evento.key) {
-            case value = 'ArrowDown':
+            case 'ArrowDown':
                 evento.preventDefault();
 
                 setOpcaoFocada(focoAntigo => {
-                    if (!focoAntigo) {
+                    if (focoAntigo == null) {
                         return 0;
                     }
-
                     return focoAntigo += 1
                 })
 
 
+                break;
+
+            case 'ArrowUp':
+                evento.preventDefault();
+                setOpcaoFocada(focoAntigo => {
+                    if (!focoAntigo) {
+                        return 0;
+                    }
+                    return focoAntigo -= 1
+                })
+
+                break;
+
+            case 'Enter':
+
+                evento.preventDefault();
+                setOpcaoFocada(null)
+                alternarVisibilidade(false)
+                setOpcaoSelecionada(opcoes[opcaoFocada])
                 break;
 
             default:
@@ -97,19 +124,20 @@ export const ListaSuspensa = ({ label, opcoes }) => {
                 estaAberta={estaAberta}
                 onKeyDown={manipularTeclaDoTeclado} >
 
-                Selecione <span>{estaAberta ? '▲' : '▼'}</span>
+                {opcaoSelecionada ? opcaoSelecionada.text : 'Selecione'}  
+                <span>{estaAberta ? '▲' : '▼'}</span>
 
             </BotaoEstilizado>
 
-            {
-                estaAberta &&
-                <ListaSuspensaEstilizada>
-                    {opcoes.map((opcao, index) =>
-                        <ItemListaSuspensaEstilizada key={opcao.value} focoAtivo={index === opcaoFocada}>
-                            {opcao.text}
-                        </ItemListaSuspensaEstilizada>)}
-                </ListaSuspensaEstilizada>
-            }
-        </LabelEstilizada>
-    )
+        {estaAberta && <ListaSuspensaEstilizada>
+            {opcoes?.map((opcao, index) => <ItemListaSuspensaEstilizada
+                key={opcao.value}
+                focoAtivo={index === opcaoFocada}
+                onClick={() => setOpcaoSelecionada(opcao)}
+                >
+                {opcao.text}
+            </ItemListaSuspensaEstilizada>)}
+        </ListaSuspensaEstilizada>}
+    </LabelEstilizada>
+   )
 }
